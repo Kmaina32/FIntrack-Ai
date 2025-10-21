103c484058083fd342c8b0d0afccf2297b6388c6
kairo kairo <GMaina424@gmail.com>
2025-10-21 10:01:25 +0000

    fix this .and the adminnav  page is still not being displayed

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -1,3 +1,4 @@
+
 'use client';
 
 import { AppSidebar } from '@/components/app-sidebar';
@@ -13,6 +14,7 @@ import { useRouter } from 'next/navigation';
 import { Loader2 } from 'lucide-react';
 import { doc, getDoc } from 'firebase/firestore';
 import type { User as AppUser } from '@/lib/types';
+import { DashboardHeader } from '@/components/dashboard-header';
 
 export default function DashboardLayout({
   children,
